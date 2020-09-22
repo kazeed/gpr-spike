0bf200af73e69354580e9ef169485f9b56a39b51
Kazz <6817525+kazeed@users.noreply.github.com>
2020-09-22 14:43:10 +0200

    First commit, sample lib

---
+++ gpr-spikle-lib/Sampler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gpr_spikle_lib
+{
+    public class Sampler
+    {
+        public void Method()
+        {
+            throw new NotImplementedException("This is just a sample man!");
+        }
+    }
+}
